b13d06b9dc10bbd97d2cf1493c8557ca52018ce4
Jeff Horowitz <horowitz2009@gmail.com>
2015-12-03 22:47:00 +0200

    ships updated
     - added Vasco da Gama

--- src/com/horowitz/seaport/MainFrame.java
+++ src/com/horowitz/seaport/MainFrame.java
@@ -90,7 +90,7 @@ public class MainFrame extends JFrame {
 
 	private final static Logger LOGGER = Logger.getLogger("MAIN");
 
-	private static String APP_TITLE = "Seaport v0.30";
+	private static String APP_TITLE = "Seaport v0.30a";
 
 	private Settings _settings;
 	private Stats _stats;
