cd4da048207290a2e3b84e475bc1d6f82f32c857
Jeff Horowitz <horowitz2009@users.noreply.github.com>
2016-03-15 13:07:49 +0200

    0.63 new ships

--- src/com/horowitz/seaport/MainFrame.java
+++ src/com/horowitz/seaport/MainFrame.java
@@ -98,7 +98,7 @@ public class MainFrame extends JFrame {
 
 	private final static Logger LOGGER = Logger.getLogger("MAIN");
 
-	private static String APP_TITLE = "Seaport v0.62c";
+	private static String APP_TITLE = "Seaport v0.63";
 
 	private Settings _settings;
 	private Stats _stats;
