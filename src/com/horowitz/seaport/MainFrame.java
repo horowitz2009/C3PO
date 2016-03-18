71e1cd0f08d9ae4eb1278a60ec8cb1ba62ed812c
Jeff Horowitz <horowitz2009@users.noreply.github.com>
2016-03-18 16:45:23 +0200

    0.64 Sao Bento

--- src/com/horowitz/seaport/MainFrame.java
+++ src/com/horowitz/seaport/MainFrame.java
@@ -98,7 +98,7 @@ public class MainFrame extends JFrame {
 
 	private final static Logger LOGGER = Logger.getLogger("MAIN");
 
-	private static String APP_TITLE = "Seaport v0.63";
+	private static String APP_TITLE = "Seaport v0.64";
 
 	private Settings _settings;
 	private Stats _stats;
