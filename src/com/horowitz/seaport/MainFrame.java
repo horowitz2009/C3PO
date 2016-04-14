428de85b598c9b494c868a916c136bd7b7152f1e
Jeff Horowitz <horowitz2009@users.noreply.github.com>
2016-04-14 19:35:34 +0300

    0.75 cannon balls

--- src/com/horowitz/seaport/MainFrame.java
+++ src/com/horowitz/seaport/MainFrame.java
@@ -93,7 +93,7 @@ public class MainFrame extends JFrame {
 
 	private final static Logger LOGGER = Logger.getLogger("MAIN");
 
-	private static String APP_TITLE = "Seaport v0.74a";
+	private static String APP_TITLE = "Seaport v0.75";
 
 	private Settings _settings;
 	private Stats _stats;
