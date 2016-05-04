7bbcc664b6726ba413b73fd6bb7c443cfb1856ce
Jeff Horowitz <horowitz2009@users.noreply.github.com>
2016-05-04 16:51:02 +0300

    0.78sss new ship and dest
    Meag Delight
    
    Exploration 2 EX2

--- src/com/horowitz/seaport/MainFrame.java
+++ src/com/horowitz/seaport/MainFrame.java
@@ -94,7 +94,7 @@ public class MainFrame extends JFrame {
 
 	private final static Logger LOGGER = Logger.getLogger("MAIN");
 
-	private static String APP_TITLE = "Seaport v0.78";
+	private static String APP_TITLE = "Seaport v0.78sss";
 
 	private Settings _settings;
 	private Stats _stats;
