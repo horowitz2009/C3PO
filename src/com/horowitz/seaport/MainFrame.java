4518065f3fa845eca6dace507e227d053967282b
Jeff Horowitz <horowitz2009@users.noreply.github.com>
2016-05-10 18:08:15 +0300

    0.80 ships images can be read outside the jar
    in folder ships

--- src/com/horowitz/seaport/MainFrame.java
+++ src/com/horowitz/seaport/MainFrame.java
@@ -94,7 +94,7 @@ public class MainFrame extends JFrame {
 
 	private final static Logger LOGGER = Logger.getLogger("MAIN");
 
-	private static String APP_TITLE = "Seaport v0.79b";
+	private static String APP_TITLE = "Seaport v0.80";
 
 	private Settings _settings;
 	private Stats _stats;
