8a32c6aca0578375bd4d05f5acc7b5e264137eb1
Jeff Horowitz <horowitz2009@gmail.com>
2016-10-25 22:53:28 +0300

    100b small.bmp new version
    TODO probably shipwreck need to changed too

--- src/com/horowitz/seaport/MainFrame.java
+++ src/com/horowitz/seaport/MainFrame.java
@@ -98,7 +98,7 @@ public class MainFrame extends JFrame {
 
 	private final static Logger LOGGER = Logger.getLogger("MAIN");
 
-	private static String APP_TITLE = "Seaport v0.100";
+	private static String APP_TITLE = "Seaport v0.100b";
 
 	private Settings _settings;
 	private Stats _stats;
